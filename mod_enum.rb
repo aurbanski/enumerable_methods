284fa986f332474dcc1e2e35a5964b0d13b762c0
aurbanski <aurbanski27@gmail.com>
2017-02-10 19:42:39 -0700

    Finished my_count and my_map

--- mod_enum.rb
+++ mod_enum.rb
@@ -116,6 +116,98 @@ module Enumerable
     return true
   end
 
+  def my_count
+      count = 0
+      #Check to see if the object is a hash
+      if self.class == Hash
+
+          #Convert to an array and start a for loop
+          hash = self.to_a
+          for i in 0..(hash.size - 1)
+
+              #Have a statement store the result of the block only if a block
+              #is given
+              statement = yield(hash[i][0], hash[i][1]) if block_given?
+
+              #If the block is given do this
+              if statement != nil
+                  #Only increment if the statement is true
+                  if statement == true
+                      count += 1
+                  end
+              #If no block is given, just give increment the count so that you
+              #can return the size of the hash
+              else
+                count += 1
+            end
+          end
+      #if the object is an array
+      else
+          arr = self.to_a
+          for i in 0..(arr.length - 1)
+              statement = yield(arr[i]) if block_given?
+
+              if statement != nil
+                 if statement == true
+                     count += 1
+                 end
+              else
+                 count += 1
+              end
+          end
+      end
+      return count
+  end
+
+  def my_map
+    if self.class == Hash
+      hash = self.to_a
+      hash2 = {}
+
+      for i in 0..(hash.size - 1)
+        element = yield(hash[i][0], hash[i][1])
+        hash2[hash[i][0]] = element
+      end
+
+      return hash2
+    else
+      arr = self.to_a
+      arr2 = []
+
+      for i in 0..(arr.length - 1)
+        element = yield (arr[i])
+        arr2 << element
+      end
+
+      return arr2
+    end
+  end
+
+  def my_inject
+    if self.class == Hash
+      hash = self.to_a
+      hash2 = {}
+
+      for i in 0..(hash.size - 1)
+        element = yield(hash[i][0], hash[i][1])
+        hash2[hash[i][0]] = element
+      end
+
+      return hash2
+    else
+      arr = self.to_a
+
+      for i in 0..(arr.length - 1)
+        if i == 0
+          element = yield (arr[0])
+        else
+          element = yield (element)
+        end
+      end
+
+      return element
+    end
+  end
 
 end
 
@@ -171,3 +263,33 @@ falsy = [2,3,6,8].my_none? do |num|
   num == 2
 end
 puts falsy
+
+#test my_count
+truthy = [2,4,6,8].my_count
+puts truthy
+
+falsy = {:alex => "Urbanski", :my => "name is", :this => "is fun"}.my_count
+puts falsy
+
+truthy = [2,4,6,8].my_count do |num|
+  num % 2 == 0
+end
+puts truthy
+
+falsy = {:alex => "Urbanski", :my => "name is", :this => "is fun"}.my_count do |key, value|
+  key == :alex
+end
+puts falsy
+
+#test my_map
+truthy = [2,4,6,8].my_map do |num|
+  num * 2
+end
+puts truthy
+
+falsy = {:alex => "Urbanski", :my => "name is", :this => "is fun"}.my_map do |key, value|
+  value + "Hell yeah"
+end
+puts falsy
+
+#test my_inject
